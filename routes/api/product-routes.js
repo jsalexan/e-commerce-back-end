e5d826a8fc6ef13c9b8903da0459efe0eca2859c
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-13 15:03:44 -0800

    Added a bit more work on the routes. In the middle of the create code for Product.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,57 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+    include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    Product.create({
+      
+        product_name: req.body.product_name,
+        price: req.body.price,
+        stock: req.body.stock,
+        tagIds: req.body.tagIds
+      })
+      .then((product) => {
+        res.json(product);
+      })
+      // TODO: Pick up here with this create code.
+    } catch (err) {
+      res.json(err);
+    }
+});
   /* req.body should look like this...
     {
       product_name: "Basketball",
