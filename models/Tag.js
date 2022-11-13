69ef4e405d888fed46216fdf9b61ac6ddc8621c6
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-13 12:56:18 -0800

    Models are done. Need to check back on one TODO in the Tag model. Otherwise, committing to move on.

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,17 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+      // TODO: Not sure if this should be here. Possibly ok to allowNull? Check back...
+    }
   },
   {
     sequelize,
